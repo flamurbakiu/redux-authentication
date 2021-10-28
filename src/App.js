26951db977a713f8c2ce440d8229970aaf3bedff
Flamur <flamurbakiu.15@gmail.com>
2021-10-28 15:58:40 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Header from './components/Header';
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {isAuthenticated ? <UserProfile /> : <Auth />}
+      {isAuthenticated && <Counter />}
+    </Fragment>
+  );
+}
+
+export default App;
